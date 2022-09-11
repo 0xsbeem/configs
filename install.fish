8a1a39a2777e5ba7608c53fc23abed5a00247b31
0xsbeem <c1ihvzhz@4wrd.cc>
2022-09-11 13:05:12 -0600

    Add task functionality
    * Create .tasks directory to track tasks
    * Update install script to install t, the task management tool
    * Update bashrc to have necessary aliases for managing tasks

--- install.fish
+++ install.fish
@@ -20,4 +20,14 @@ else
 	ln -sf $fullpath $XDG_CONFIG_HOME/bash/bashrc
 end
 
+echo "[t] Checking if t is installed"
+which t 2&> /dev/null
+if [ $status -eq 0 ]
+  echo "installed"
+else
+  echo "Need to install t. This may prompt you for your sudo password."
+  mkdir --parents ~/.local/t 
+  hg clone https://hg.stevelosh.com/t ~/.local/t
+  sudo ln -sf ~/.local/t/t.py /usr/local/bin/t
+end
 
