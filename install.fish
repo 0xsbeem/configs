87f43b6d379de4605ba66debaa923a582669ae74
0xsbeem <c1ihvzhz@4wrd.cc>
2022-09-11 12:27:30 -0600

    Add bashrc

--- install.fish
+++ install.fish
@@ -5,9 +5,19 @@ set os $(uname)
 echo "[nvim] Symlinking config"
 if [ "$os" = "Darwin" ]
 	set fullpath $(greadlink -f nvim/init.lua)
-	ln -sf "$fullpath" $HOME/nvim/init.lua
+	ln -sf "$fullpath" $XDG_CONFIG_HOME/nvim/init.lua
 else
 	set fullpath $(readlink -f nvim/init.lua)
 	ln -sf $fullpath $XDG_CONFIG_HOME/nvim/init.lua
 end
+
+echo "[bash] Symlinking config"
+if [ "$os" = "Darwin" ]
+	set fullpath $(greadlink -f bash/bashrc)
+	ln -sf "$fullpath" $XDG_CONFIG_HOME/bash/bashrc
+else
+	set fullpath $(readlink -f bash/bashrc)
+	ln -sf $fullpath $XDG_CONFIG_HOME/bash/bashrc
+end
+
 
